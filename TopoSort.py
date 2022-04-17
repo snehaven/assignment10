2435952a2fe5beb7bc40a199076a10b7ae091393
snehaven <65678267+snehaven@users.noreply.github.com>
2022-04-17 16:11:58 -0500

    Update TopoSort.py

--- TopoSort.py
+++ TopoSort.py
@@ -197,7 +197,7 @@ class Graph(object):
 
     def has_cycle(self):
         for item in self.Vertices:
-            if (item.was_visited() && self.dfs(item)):
+            if (item.was_visited() and self.dfs(item)):
                 return True
 
         return False
